1d92ffda024786b5301fc893367a8109011b9824
harell <harel@loostiger.com>
2021-06-03 16:23:53 +1200

    load package

--- tests/testthat/helpers-xyz.R
+++ tests/testthat/helpers-xyz.R
@@ -6,11 +6,14 @@ test_http <- function(desc, code){
         on.exit(httptest::.mockPaths(old_dir))
         httptest::.mockPaths("_api")
         testthat::test_that(desc, {
-            suppressWarnings(tryCatch(
-                suppressMessages(httptest::with_mock_api(code)),
-                error = function(e) return(httptest::capture_requests(code))
-            ))
+                suppressWarnings(tryCatch(
+                        suppressMessages(httptest::with_mock_api(code)),
+                        error = function(e) return(httptest::capture_requests(code))
+                ))
         })
 }
 
-pkg_name <- function() getPackageName(search()[max(which(search() %in% c(".GlobalEnv", "devtools_shims")))+1])
+pkg_name <- function() tryCatch(
+        pkgload::pkg_name(),
+        error = function(e) return(getPackageName(search()[max(which(search() %in% c(".GlobalEnv", "devtools_shims")))+1]))
+)
