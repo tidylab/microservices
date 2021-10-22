ee1dccb8ba8f589782df1f8d4edc346219f428bf
harell <harel@loostiger.com>
2021-10-23 12:48:40 +1300

    update infrastructure

--- .dev/CRAN/prepare-for-release.R
+++ .dev/CRAN/prepare-for-release.R
@@ -1,3 +1,7 @@
+# Configuration -----------------------------------------------------------
+Sys.setenv(`_R_DEPENDS_ONLY` = "true")
+
+
 # Setup -------------------------------------------------------------------
 remotes::install_cran(c("devtools", "urlchecker", "rhub", "revdepcheck"))
 # remotes::install_github("r-lib/revdepcheck@master")
@@ -11,3 +15,7 @@ devtools::check_win_devel()
 rhub::check_for_cran()
 revdepcheck::revdep_check(num_workers = 8)
 # revdepcheck::revdep_reset(pkg = ".")
+
+
+# Deployment --------------------------------------------------------------
+# devtools::submit_cran()
---
+++ .dev/hooks/tidy-styling.R
@@ -0,0 +1,21 @@
+# usethis::use_tidy_github_actions()
+#
+# usethis::create_tidy_package(path, copyright_holder = NULL)
+#
+usethis::use_tidy_description()
+#
+# usethis::use_tidy_eval()
+#
+# usethis::use_tidy_contributing()
+#
+# usethis::use_tidy_support()
+#
+# usethis::use_tidy_issue_template()
+#
+# usethis::use_tidy_coc()
+#
+# usethis::use_tidy_github()
+#
+# usethis::use_tidy_style(strict = TRUE)
+#
+# usethis::use_tidy_release_test_env()
